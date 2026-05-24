2c44016fe06bc4ab2faef8ab2348eddec11555f4
Oleg Khilko <khilkoleg@gmail.com>
2026-05-24 12:24:32 +0300

    Initial Spring Boot setup

---
+++ src/main/java/com/monadacademy/backend/MonadAcademyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.monadacademy.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonadAcademyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonadAcademyBackendApplication.class, args);
+	}
+
+}
