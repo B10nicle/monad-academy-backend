9d486741dab69f1c17db3c7709264fd7489fc088
Oleg Khilko <khilkoleg@gmail.com>
2026-05-24 17:39:48 +0300

    Order test class annotations by length

--- src/test/java/com/monadacademy/backend/MonadAcademyBackendApplicationTests.java
+++ src/test/java/com/monadacademy/backend/MonadAcademyBackendApplicationTests.java
@@ -9,9 +9,9 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 @ActiveProfiles("test")
-@Testcontainers
 class MonadAcademyBackendApplicationTests {
 
 	@Container
